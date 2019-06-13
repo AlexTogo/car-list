36f947b0c7566e9be8bd7a8c84e6497d833f40de
AlexTogo <ImAlexTogo@gmail.com>
2019-06-12 22:32:02 -0400

    chore: added submit function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.term);
+    }
+
     render() {
         return (
             <div className='ui segment'>
-                <form className='ui form'>
+                <form onSubmit={this.onFormSubmit} className='ui form'>
                     <div className='field'>
                         <label>Search</label>
-                        <input text='text' />
+                        {/* 
+                            -not put parentheses onChange function
+                            -Controlled Component via State much better
+                        */}
+                        <input text='text' value={this.state.term} onChange={(e) => this.setState({ term: e.target.value })} />
                     </div>
                 </form>
             </div>
